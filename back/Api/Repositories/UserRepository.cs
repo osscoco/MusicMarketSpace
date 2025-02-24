747660054a1913bffe5382ab03b275b3dc6eeda4
Corentin JEANNOT <corentin.jeannot2a@gmail.com>
2025-02-24 15:39:36 +0100

    Ajout de la méthode Get All User #12

---
+++ back/Api/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using back.Dtos.Responses;
+using back.Interfaces;
+using EFCore;
+using Microsoft.AspNetCore.Mvc;
+using Models.Common;
+
+namespace back.Repositories
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult<ResponseApi<object>> GetAllUsers()
+        {
+            var users = _context.Users
+                .Select(user => new UserResponse
+                {
+                    Pseudo = user.Pseudo,
+                    Mail = user.Mail,
+                    ContactPhone = user.ContactPhone,
+                    IsSSO = user.IsSSO,
+                    RoleId = user.RoleId
+                })
+                .ToList();
+
+            return new ResponseApi<object>
+            {
+                Success = true,
+                Data = users,
+                Message = "Utilisateurs chargés !"
+            };
+        }
+    }
+}
