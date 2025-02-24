856b64dc06bc09ef6feb866ab0bf0d74a17fdafd
Corentin JEANNOT <corentin.jeannot2a@gmail.com>
2025-02-25 00:57:06 +0100

    Ajout de la méthode Create One User #14

--- back/Api/Repositories/UserRepository.cs
+++ back/Api/Repositories/UserRepository.cs
@@ -1,20 +1,30 @@
-﻿using back.Dtos.Responses;
+﻿using back.Dtos.Requests;
+using back.Dtos.Responses;
 using back.Interfaces;
 using EFCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models.Common;
+using Models.Identity;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace back.Repositories
 {
     public class UserRepository: IUserRepository
     {
+        #region Attributs
         private readonly AppDbContext _context;
+        #endregion
 
+        #region Constructeur
         public UserRepository(AppDbContext context)
         {
             _context = context;
         }
+        #endregion
 
+        #region GetAllUsers
         public ActionResult<ResponseApi<object>> GetAllUsers()
         {
             var users = _context.Users
@@ -35,5 +45,57 @@ namespace back.Repositories
                 Message = "Utilisateurs chargés !"
             };
         }
+        #endregion
+
+        #region CreateOneUser
+        public async Task<ActionResult<ResponseApi<object>>> CreateOneUser(UserCreateRequest user)
+        {
+            if (await this.UserExistsByEmail(user.Mail))
+            {
+                return new ResponseApi<object>(false, null, "L'utilisateur existe déjà");
+            }
+
+            User createdUser = new User
+            {
+                Pseudo = user.Pseudo,
+                Mail = user.Mail,
+                PasswordHashed = HashString(user.Password),
+                ContactPhone = user.ContactPhone,
+                IsSSO = user.IsSSO,
+                RoleId = user.RoleId
+            };
+
+            _context.Users.Add(createdUser);
+            await _context.SaveChangesAsync();
+
+            return new ResponseApi<object>(true, null, "Utilisateur créé avec succès");
+        }
+        #endregion
+
+        #region Functions personnalisées
+        public async Task<bool> UserExistsByEmail(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Mail == email);
+        }
+
+        public string HashString(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+        #endregion
     }
 }
---
+++ back/Api/Services/TranslateException.cs
@@ -0,0 +1,16 @@
+﻿namespace back.Services
+{
+    public class TranslateException
+    {
+        public string TranslateExceptionEnToFr(Exception ex)
+        {
+            switch (ex.Message)
+            {
+                case "An error occurred while saving the entity changes. See the inner exception for details.":
+                    return "Une erreur s'est produite lors de l'enregistrement des modifications apportées à l'entité. Consultez l'exception interne pour plus de détails.";
+                default:
+                    return $"Erreur inconnue : {ex.Message}";
+            }
+        }
+    }
+}
